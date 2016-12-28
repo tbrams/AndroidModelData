aa74cd6ae24e1376c79b823d3f9c3beccf7f4542
tbrams <torben@brams.dk>
2016-12-28 13:57:41 +0100

    ListView complete

--- app/src/main/java/com/example/android/data/MainActivity.java
+++ app/src/main/java/com/example/android/data/MainActivity.java
@@ -2,42 +2,40 @@ package com.example.android.data;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.android.data.model.DataItem;
 import com.example.android.data.model.SampleDataProvider;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+
+import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvOut;
     List<DataItem> dataItemList = SampleDataProvider.sDataItemList;
-
+    List<String> itemNames = new ArrayList<>();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            tvOut = (TextView) findViewById(R.id.out);
-            tvOut.setText("");
-
-            // Sort items alphabetically
-            Collections.sort(dataItemList, new Comparator<DataItem>() {
-                @Override
-                public int compare(DataItem dataItem, DataItem t1) {
-                    return dataItem.getItemName().compareTo(t1.getItemName());
-                }
-            });
-
             for (DataItem item :
                     dataItemList) {
-                tvOut.append(item.getItemName()+"\n");
-
+                itemNames.add(item.getItemName());
             }
+            Collections.sort(itemNames);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemNames);
+            ListView listView = (ListView) findViewById(list);
+            listView.setAdapter(adapter);
 
         }
 }
+
