7bf61b4496af6db801b3166f5e92381f792572c0
tbrams <torben@brams.dk>
2016-12-28 13:23:44 +0100

    Menu data populated with sample data and output soorted alphabetically

--- app/src/main/java/com/example/android/data/MainActivity.java
+++ app/src/main/java/com/example/android/data/MainActivity.java
@@ -5,10 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.android.data.model.DataItem;
+import com.example.android.data.model.SampleDataProvider;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-        TextView tvOut;
+    TextView tvOut;
+    List<DataItem> dataItemList = SampleDataProvider.sDataItemList;
 
 
         @Override
@@ -16,9 +22,22 @@ public class MainActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            DataItem item = new DataItem(null, "name", "description", "category", 1, 9.95, "imagename.jpg");
             tvOut = (TextView) findViewById(R.id.out);
-            tvOut.setText(item.toString());
+            tvOut.setText("");
+
+            // Sort items alphabetically
+            Collections.sort(dataItemList, new Comparator<DataItem>() {
+                @Override
+                public int compare(DataItem dataItem, DataItem t1) {
+                    return dataItem.getItemName().compareTo(t1.getItemName());
+                }
+            });
+
+            for (DataItem item :
+                    dataItemList) {
+                tvOut.append(item.getItemName()+"\n");
+
+            }
 
         }
 }
